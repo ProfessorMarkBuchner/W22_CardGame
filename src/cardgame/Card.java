c79ad9d0fe054327cb68b43ae1290b211ea85458
16476 <16476@10.0.0.64>
2022-02-03 18:34:51 -0500

    First Commit: Code change in CardGame.java

---
+++ src/cardgame/Card.java
@@ -0,0 +1,56 @@
+package cardgame;
+/**
+ *This class  name is Card.java
+ *
+ * @author Arunpal Singh 
+ * 3 Feburary 2022
+ * Version 1.0
+ *  Submitted to Mark Buchner
+ */
+
+public class Card {
+    public enum Suit {
+        HEARTS, CLUBS, SPADES, DIAMONDS, STAR
+      }
+    
+    public enum Value {
+            ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
+            EIGHT, NINE, TEN, JACK, QUEEN, KING, EMPEROR
+       }
+    
+    private Value value;
+    private Suit suit;
+    
+
+    public Card(Value value, Suit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    
+    
+    public Value getValue() {
+        return this.value;
+    }
+
+    /**
+     * 
+     * @param value
+     */
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    /**
+     * 
+     * @param suit
+     */
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+}
