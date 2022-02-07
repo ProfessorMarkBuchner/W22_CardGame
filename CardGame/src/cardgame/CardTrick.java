580a04524286507fdb487864aa5a3a5a461b90e3
Sukhmandeep kaur <kau10958@sheridancollege.ca>
2022-02-06 20:29:04 -0500

    Added new class called CardTrick and added some methods that are invoked in CardGame.java in class

--- CardGame/src/cardgame/CardTrick.java
+++ CardGame/src/cardgame/CardTrick.java
@@ -8,5 +8,23 @@ package cardgame;
  * @author Sukhmandeep Kaur
  */
 public class CardTrick{
-
+    private String suitOfCard;
+    private int valueOfCard;
+    
+    //gettters
+    public String getSuit(){
+        return suitOfCard;
+    }
+    public int getValue(){
+        return valueOfCard;
+    }
+    
+    public static String suits[]={"Heart","Club","Spade","Diamond"};
+    
+    //constructor added here that takes two parameters 
+    public CardTrick(String suit,int value){
+        suitOfCard=suit;
+        valueOfCard=value;
+    }
+    
 }
